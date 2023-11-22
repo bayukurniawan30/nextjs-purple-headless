9129315cd27c3499db11a41a49a0812b1cbe8f28
Bayu Kurniawan <bayu.kurniawan@taksu.tech>
2023-11-22 10:21:45 +0800

    test cors again

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,34 +1,35 @@
-import Axios from "axios";
-import { enqueueSnackbar } from "notistack";
+import Axios from 'axios'
+import { enqueueSnackbar } from 'notistack'
 
-const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL
 
 const axios = Axios.create({
   withCredentials: true,
   headers: {
-    "X-Requested-With": "XMLHttpRequest",
+    'X-Requested-With': 'XMLHttpRequest',
+    'Access-Control-Allow-Origin': '*',
   },
   baseURL: baseURL,
-});
+})
 
-if (typeof window !== "undefined" && localStorage.getItem("token")) {
-  const token = localStorage.getItem("token");
-  axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
+if (typeof window !== 'undefined' && localStorage.getItem('token')) {
+  const token = localStorage.getItem('token')
+  axios.defaults.headers.common = { Authorization: `Bearer ${token}` }
 }
 
 axios.interceptors.response.use(
   (response) => {
-    return response;
+    return response
   },
   (error) => {
     enqueueSnackbar(`Failed to process your request. Please try again`, {
-      variant: "error",
-      anchorOrigin: { horizontal: "right", vertical: "bottom" },
-    });
+      variant: 'error',
+      anchorOrigin: { horizontal: 'right', vertical: 'bottom' },
+    })
 
     // Trigger an alert here (e.g., using a notification library or custom function)
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
-export default axios;
+export default axios
