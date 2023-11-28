d7f097b50f2371b03bb2bdde6a1e9c9808d738c1
Bayu Kurniawan <bayu.kurniawan@taksu.tech>
2023-11-28 17:26:34 +0800

    test use persist storage zustand

--- src/hooks/settings.ts
+++ src/hooks/settings.ts
@@ -1,8 +1,8 @@
 // settingsStore.js
 import axios, { baseURL } from '@/lib/axios'
 import { Setting } from '@/type/api'
-import Axios from 'axios'
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface SettingsState {
   settings: Setting[]
@@ -11,75 +11,59 @@ interface SettingsState {
   getSettingByKey: (key: string) => Setting | null
 }
 
-// Need to re-create Axios instance, because the auth cannot be injected here
-// const axios = Axios.create({
-//   withCredentials: true,
-//   headers: {
-//     'X-Requested-With': 'XMLHttpRequest',
-//   },
-//   baseURL: baseURL,
-// })
+const useSettingsStore = create<SettingsState>(
+  persist(
+    (set) => ({
+      settings: [],
+      fetchSettings: async () => {
+        try {
+          const response = await axios.get('/settings', {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+          })
+          set({ settings: response.data.data })
+        } catch (error) {
+          console.error('Error fetching settings:', error)
+        }
+      },
+      updateSetting: async (newSetting) => {
+        try {
+          // Make a request to update the settings on the server
+          // For simplicity, let's assume a hypothetical update endpoint
+          await axios.put(`/settings/${newSetting.id}`, newSetting, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+          })
 
-// if (typeof window !== 'undefined' && localStorage.getItem('token')) {
-//   const token = localStorage.getItem('token')
-//   axios.defaults.headers.common = { Authorization: `Bearer ${token}` }
-// }
-
-const useSettingsStore = create<SettingsState>((set) => ({
-  settings: [],
-  fetchSettings: async () => {
-    try {
-      const storedSettings = localStorage.getItem('settings')
-      if (storedSettings) {
-        set({ settings: JSON.parse(storedSettings) })
-      } else {
-        const response = await axios.get('/settings', {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-        })
-        set({ settings: response.data.data })
-        localStorage.setItem('settings', JSON.stringify(response.data.data))
-      }
-    } catch (error) {
-      console.error('Error fetching settings:', error)
+          // Update the state with the new settings
+          set((state) => ({
+            settings: state.settings.map((setting) =>
+              setting.id === newSetting.id ? newSetting : setting
+            ),
+          }))
+        } catch (error) {
+          console.error('Error updating settings:', error)
+        }
+      },
+      getSettingByKey: (key) => {
+        // Get a specific setting by its key
+        useSettingsStore.getState().fetchSettings()
+        return (
+          (useSettingsStore.getState().settings || []).find(
+            (setting: Setting) => setting.key === key
+          ) || null
+        )
+      },
+    }),
+    {
+      name: 'settings-storage',
+      storage: () => localStorage,
+      partialize: (state) => ({ settings: state.settings }),
     }
-  },
-  updateSetting: async (newSetting) => {
-    try {
-      // Make a request to update the settings on the server
-      // For simplicity, let's assume a hypothetical update endpoint
-      await axios.put(`/settings/${newSetting.id}`, newSetting, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      })
-
-      // Update the state with the new settings
-      // and update settings in local storage
-      set((state) => ({
-        settings: state.settings.map((setting) =>
-          setting.id === newSetting.id ? newSetting : setting
-        ),
-      }))
-
-      // Update settings in local storage
-      const updatedSettings = useSettingsStore.getState().settings
-      localStorage.setItem('settings', JSON.stringify(updatedSettings))
-    } catch (error) {
-      console.error('Error updating settings:', error)
-    }
-  },
-  getSettingByKey: (key): Setting | null => {
-    // Get a specific setting by its key
-    useSettingsStore.getState().fetchSettings()
-    return (
-      (useSettingsStore.getState().settings || []).find(
-        (setting: Setting) => setting.key === key
-      ) || null
-    )
-  },
-}))
+  )
+)
 
 // Create a singleton instance of the store
 export { useSettingsStore }
