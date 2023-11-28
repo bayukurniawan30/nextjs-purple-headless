0bcbf206df0d2e24ceaab48123a3fbd79cf04f42
Bayu Kurniawan <bayu.kurniawan@taksu.tech>
2023-11-28 11:38:37 +0800

    update axios header

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -11,13 +11,21 @@ const axios = Axios.create({
   baseURL: baseURL,
 })
 
-if (typeof window !== 'undefined' && localStorage.getItem('token')) {
-  const token = localStorage.getItem('token')
-  axios.defaults.headers.common = { Authorization: `Bearer ${token}` }
-}
+// if (typeof window !== 'undefined' && localStorage.getItem('token')) {
+//   const token = localStorage.getItem('token')
+//   axios.defaults.headers.common = { Authorization: `Bearer ${token}` }
+
+// }
 
 axios.interceptors.response.use(
   (response) => {
+    const token = localStorage.getItem('token') // replace with your actual key
+
+    // Set the Bearer token in the request headers
+    if (token) {
+      response.headers.Authorization = `Bearer ${token}`
+    }
+
     return response
   },
   (error) => {
